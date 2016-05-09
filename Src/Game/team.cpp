a94d06f69c09461bff4012ecd12843341a5869e2
lee jaejung <jjuiddong@gmail.com>
2016-05-09 21:15:40 +0900

    update team ai

--- Src/Game/team.cpp
+++ Src/Game/team.cpp
@@ -7,6 +7,7 @@ using namespace soccer;
 
 
 cTeam::cTeam()
+	: ai::cActor()
 {
 }
 
--- Src/Game/team.h
+++ Src/Game/team.h
@@ -1,6 +1,8 @@
 //
 // 2016-05-01, jjuiddong
+//
 // 축구 팀을 표현하는 객체
+// 순수하게 인공지능 관련된 일만 처리한다.
 //
 #pragma once
 
@@ -9,7 +11,7 @@ namespace soccer
 {
 	class cPlayer;
 
-	class cTeam
+	class cTeam : public ai::cActor
 	{
 	public:
 		cTeam();
@@ -18,7 +20,6 @@ namespace soccer
 		bool Init(graphic::cRenderer &renderer);
 		bool Update(const float deltaSeconds);
 		void Render(graphic::cRenderer &renderer, const Matrix44 &tm= Matrix44::Identity);
-
 		void Clear();
 
 
